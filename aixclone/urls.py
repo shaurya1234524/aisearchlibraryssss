ee0473791eb9f127e7b6c3ba90837e2eec9efcd0
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-10-05 22:03:19 +0530

    Update urls.py

--- aixclone/urls.py
+++ aixclone/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
         # ... your normal URLs ...
      path('', ToolListView.as_view(), name='list'),
     path('submit/', views.submit_tool, name='submit'),
-
+        path('sitemap.xml',views.sitemap,name="sitemap"),
+ path('robots.txt',views.robots,name="robots"),
 path('admin/',admin.site.urls),
       path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
